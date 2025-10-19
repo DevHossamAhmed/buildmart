7f49b3f4146ebbd6d887097cd938e548f98fbda2
Mahmoud Ahmed <developer.mahmoudahmed@gmail.com>
2025-10-20 01:55:55 +0300

    update

--- src/app/auth/sign-up/page.tsx
+++ src/app/auth/sign-up/page.tsx
@@ -1,17 +1,25 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 "use client"
 import React, { useState } from 'react';
-import  logo  from '../../../../public/assets/images/logo.jpg';
+import logo from '../../../../public/assets/images/logo.jpg';
 import Image from 'next/image';
 import Link from 'next/link';
 
 const SignupPage = () => {
   const [userType, setUserType] = useState('Individual');
+  const [entityType, setEntityType] = useState('Holding');
   const [formData, setFormData] = useState({
+    entityType: '',
+    brandName: '',
+    website: '',
+    logo: '',
+    telephone: '',
     firstName: '',
     lastName: '',
     email: '',
+    mobile: '',
     password: '',
+    confirmPassword: '',
     companyName: '',
     taxId: '',
     businessName: '',
@@ -36,12 +44,12 @@ const SignupPage = () => {
         {/* Logo */}
         <div className="flex justify-center mb-8">
           <div className="">
-            <Image 
-            src={logo}
-            alt='logo'
-            width={100}
+            <Image
+              src={logo}
+              alt='logo'
+              width={100}
               height={30}
-              className="w-auto h-auto"/>
+              className="w-auto h-auto" />
           </div>
         </div>
 
@@ -51,7 +59,7 @@ const SignupPage = () => {
         <div className="space-y-5">
           {/* User Type Dropdown */}
           <div>
-            <label className="block text-sm font-medium text-gray-700 mb-2">User Type</label>
+            <label className="block text-sm font-medium text-gray-700 mb-2">User Type*</label>
             <select
               value={userType}
               onChange={(e) => setUserType(e.target.value)}
@@ -62,114 +70,20 @@ const SignupPage = () => {
                 backgroundPosition: 'right 1rem center'
               }}
             >
-              <option value="Individual">Individual</option>
-              <option value="Entity">Entity</option>
+              <option value="Individual">Buyer Individual</option>
+              <option value="Buyer Entity">Buyer Entity</option>
               <option value="Vendor">Vendor</option>
             </select>
           </div>
-
-          {/* Individual Fields */}
-          {userType === 'Individual' && (
-            <>
-              <div className="grid grid-cols-2 gap-4">
-                <div>
-                  <label className="block text-sm font-medium text-gray-700 mb-2">First Name</label>
-                  <input
-                    type="text"
-                    name="firstName"
-                    value={formData.firstName}
-                    onChange={handleInputChange}
-                    placeholder="First Name"
-                    className="w-full px-4 py-3 border border-gray-300 rounded-lg focus:ring-2 focus:ring-gray-300focus:border-transparent transition-all"
-                  />
-                </div>
-                <div>
-                  <label className="block text-sm font-medium text-gray-700 mb-2">Last Name</label>
-                  <input
-                    type="text"
-                    name="lastName"
-                    value={formData.lastName}
-                    onChange={handleInputChange}
-                    placeholder="Last Name"
-                    className="w-full px-4 py-3 border border-gray-300 rounded-lg focus:ring-2 focus:ring-gray-300 focus:border-transparent transition-all"
-                  />
-                </div>
-              </div>
-            </>
-          )}
-
           {/* Entity Fields */}
-          {userType === 'Entity' && (
+          {userType != 'Individual' && (
             <>
               <div>
-                <label className="block text-sm font-medium text-gray-700 mb-2">Company Name</label>
-                <input
-                  type="text"
-                  name="companyName"
-                  value={formData.companyName}
-                  onChange={handleInputChange}
-                  placeholder="Company Name"
-                  className="w-full px-4 py-3 border border-gray-300 rounded-lg focus:ring-2 focus:ring-gray-300 focus:border-transparent transition-all"
-                />
-              </div>
-              <div>
-                <label className="block text-sm font-medium text-gray-700 mb-2">Tax ID Number</label>
-                <input
-                  type="text"
-                  name="taxId"
-                  value={formData.taxId}
-                  onChange={handleInputChange}
-                  placeholder="Tax ID Number"
-                  className="w-full px-4 py-3 border border-gray-300 rounded-lg focus:ring-2 focus:ring-gray-300 focus:border-transparent transition-all"
-                />
-              </div>
-              <div className="grid grid-cols-2 gap-4">
-                <div>
-                  <label className="block text-sm font-medium text-gray-700 mb-2">Legal Representative</label>
-                  <input
-                    type="text"
-                    name="firstName"
-                    value={formData.firstName}
-                    onChange={handleInputChange}
-                    placeholder="Representative"
-                    className="w-full px-4 py-3 border border-gray-300 rounded-lg focus:ring-2 focus:ring-gray-300 focus:border-transparent transition-all"
-                  />
-                </div>
-                <div>
-                  <label className="block text-sm font-medium text-gray-700 mb-2">Position</label>
-                  <input
-                    type="text"
-                    name="lastName"
-                    value={formData.lastName}
-                    onChange={handleInputChange}
-                    placeholder="Position"
-                    className="w-full px-4 py-3 border border-gray-300 rounded-lg focus:ring-2 focus:ring-gray-300 focus:border-transparent transition-all"
-                  />
-                </div>
-              </div>
-            </>
-          )}
-
-          {/* Vendor Fields */}
-          {userType === 'Vendor' && (
-            <>
-              <div>
-                <label className="block text-sm font-medium text-gray-700 mb-2">Business Name</label>
-                <input
-                  type="text"
-                  name="businessName"
-                  value={formData.businessName}
-                  onChange={handleInputChange}
-                  placeholder="Business Name"
-                  className="w-full px-4 py-3 border border-gray-300 rounded-lg focus:ring-2 focus:ring-gray-300 focus:border-transparent transition-all"
-                />
-              </div>
-              <div>
-                <label className="block text-sm font-medium text-gray-700 mb-2">Vendor Category</label>
+                <label className="block text-sm font-medium text-gray-700 mb-2">Choose Entity Type*</label>
                 <select
-                  name="vendorCategory"
-                  value={formData.vendorCategory}
-                  onChange={handleInputChange}
+                  name="entityType"
+                  value={entityType}
+                  onChange={(e) => setEntityType(e.target.value)}
                   className="w-full px-4 py-3 pr-10 border border-gray-300 rounded-lg focus:ring-2 focus:ring-gray-300 focus:border-transparent transition-all appearance-none bg-white"
                   style={{
                     backgroundImage: `url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='12' height='12' viewBox='0 0 12 12'%3E%3Cpath fill='%23374151' d='M6 9L1 4h10z'/%3E%3C/svg%3E")`,
@@ -177,42 +91,126 @@ const SignupPage = () => {
                     backgroundPosition: 'right 1rem center'
                   }}
                 >
-                  <option value="">Select Category</option>
-                  <option value="products">Products</option>
-                  <option value="services">Services</option>
-                  <option value="both">Both</option>
+                  <option value="Holding">Holding</option>
+                  <option value="Single Entity">Single Entity</option>
                 </select>
               </div>
-              <div className="grid grid-cols-2 gap-4">
+              <div>
+                <label className="block text-sm font-medium text-gray-700 mb-2">{entityType === 'Holding' ? 'Holding Name' : 'Company Name'}*</label>
+                <input
+                  type="text"
+                  name="companyName"
+                  value={formData.companyName}
+                  onChange={handleInputChange}
+                  placeholder="e.g. Acme Corporation"
+                  className="w-full px-4 py-3 border border-gray-300 rounded-lg focus:ring-2 focus:ring-gray-300 focus:border-transparent transition-all"
+                />
+              </div>
+              <div>
+                <label className="block text-sm font-medium text-gray-700 mb-2">Trademark / Brand Name</label>
+                <input
+                  type="text"
+                  name="brandName"
+                  value={formData.brandName}
+                  onChange={handleInputChange}
+                  placeholder="e.g. BrightTech Solutions"
+                  className="w-full px-4 py-3 border border-gray-300 rounded-lg focus:ring-2 focus:ring-gray-300 focus:border-transparent transition-all"
+                />
+              </div>
+              <div>
+                <label className="block text-sm font-medium text-gray-700 mb-2">Commercial Registration Number*</label>
+                <input
+                  type="text"
+                  name="taxId"
+                  value={formData.taxId}
+                  onChange={handleInputChange}
+                  placeholder="Enter your CR number (e.g. 1010123456)"
+                  className="w-full px-4 py-3 border border-gray-300 rounded-lg focus:ring-2 focus:ring-gray-300 focus:border-transparent transition-all"
+                />
+              </div>
+               <div className='grid grid-cols-2 gap-4'>
                 <div>
-                  <label className="block text-sm font-medium text-gray-700 mb-2">Contact Person</label>
+                  <label className="block text-sm font-medium text-gray-700 mb-2">Email Address</label>
                   <input
-                    type="text"
-                    name="firstName"
-                    value={formData.firstName}
+                    type="email"
+                    name="email"
+                    value={formData.email}
                     onChange={handleInputChange}
-                    placeholder="Contact Person"
+                    placeholder="email@example.com"
                     className="w-full px-4 py-3 border border-gray-300 rounded-lg focus:ring-2 focus:ring-gray-300 focus:border-transparent transition-all"
                   />
                 </div>
                 <div>
-                  <label className="block text-sm font-medium text-gray-700 mb-2">License Number</label>
+                  <label className="block text-sm font-medium text-gray-700 mb-2">Tel</label>
                   <input
-                    type="text"
-                    name="lastName"
-                    value={formData.lastName}
+                    type="tel"
+                    name="telephone"
+                    value={formData.telephone}
                     onChange={handleInputChange}
-                    placeholder="License Number"
+                    placeholder="e.g. +966 55 123 4567"
                     className="w-full px-4 py-3 border border-gray-300 rounded-lg focus:ring-2 focus:ring-gray-300 focus:border-transparent transition-all"
                   />
                 </div>
               </div>
+              <div>
+                <label className="block text-sm font-medium text-gray-700 mb-2">Website</label>
+                <input
+                  type="text"
+                  name="businessName"
+                  value={formData.website}
+                  onChange={handleInputChange}
+                  placeholder="e.g. https://www.acme.com"
+                  className="w-full px-4 py-3 border border-gray-300 rounded-lg focus:ring-2 focus:ring-gray-300 focus:border-transparent transition-all"
+                />
+              </div>
+              <div>
+                <label className="block text-sm font-medium text-gray-700 mb-2">Logo</label>
+                <input
+                  type="file"
+                  name="logo"
+                  value={formData.logo}
+                  onChange={handleInputChange}
+                  placeholder="Logo"
+                  className="w-full px-4 py-3 border border-gray-300 rounded-lg focus:ring-2 focus:ring-gray-300 focus:border-transparent transition-all"
+                />
+              </div>
+              <hr />
             </>
           )}
-
-          {/* Email */}
+          {userType != 'Individual' && (
+            <div className="flex items-center p-4 mb-4 text-sm text-yellow-800 rounded-lg bg-yellow-50 border border-yellow-200" role="alert">
+              <svg className="w-5 h-5 mr-2 text-yellow-600" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4a2 2 0 00-3.464 0L4.34 16c-.77 1.333.192 3 1.732 3z" />
+              </svg>
+              This person will be Superadmin to this account.
+            </div>
+          )}
+          <div className="grid grid-cols-2 gap-4">
+            <div>
+              <label className="block text-sm font-medium text-gray-700 mb-2">First Name*</label>
+              <input
+                type="text"
+                name="firstName"
+                value={formData.firstName}
+                onChange={handleInputChange}
+                placeholder="First Name"
+                className="w-full px-4 py-3 border border-gray-300 rounded-lg focus:ring-2 focus:ring-gray-300focus:border-transparent transition-all"
+              />
+            </div>
+            <div>
+              <label className="block text-sm font-medium text-gray-700 mb-2">Last Name*</label>
+              <input
+                type="text"
+                name="lastName"
+                value={formData.lastName}
+                onChange={handleInputChange}
+                placeholder="Last Name"
+                className="w-full px-4 py-3 border border-gray-300 rounded-lg focus:ring-2 focus:ring-gray-300 focus:border-transparent transition-all"
+              />
+            </div>
+          </div>
           <div>
-            <label className="block text-sm font-medium text-gray-700 mb-2">Email Address</label>
+            <label className="block text-sm font-medium text-gray-700 mb-2">{ userType != 'Individual' ? 'Official Email Address' : 'Email Address' }*</label>
             <input
               type="email"
               name="email"
@@ -222,10 +220,19 @@ const SignupPage = () => {
               className="w-full px-4 py-3 border border-gray-300 rounded-lg focus:ring-2 focus:ring-gray-300 focus:border-transparent transition-all"
             />
           </div>
-
-          {/* Password */}
           <div>
-            <label className="block text-sm font-medium text-gray-700 mb-2">Password</label>
+            <label className="block text-sm font-medium text-gray-700 mb-2">{ userType != 'Individual' ? 'Official Mobile' : 'Mobile' }*</label>
+            <input
+              type="tel"
+              name="mobile"
+              value={formData.mobile}
+              onChange={handleInputChange}
+              placeholder="e.g. +966 55 123 4567"
+              className="w-full px-4 py-3 border border-gray-300 rounded-lg focus:ring-2 focus:ring-gray-300 focus:border-transparent transition-all"
+            />
+          </div>
+          <div>
+            <label className="block text-sm font-medium text-gray-700 mb-2">Password*</label>
             <input
               type="password"
               name="password"
@@ -235,8 +242,17 @@ const SignupPage = () => {
               className="w-full px-4 py-3 border border-gray-300 rounded-lg focus:ring-2 focus:ring-gray-300 focus:border-transparent transition-all"
             />
           </div>
-
-          {/* Submit Button */}
+          <div>
+            <label className="block text-sm font-medium text-gray-700 mb-2">Confirm Password*</label>
+            <input
+              type="password"
+              name="confirm_password"
+              value={formData.confirmPassword}
+              onChange={handleInputChange}
+              placeholder="Enter your confirm-password"
+              className="w-full px-4 py-3 border border-gray-300 rounded-lg focus:ring-2 focus:ring-gray-300 focus:border-transparent transition-all"
+            />
+          </div>
           <button
             onClick={handleSubmit}
             className="w-full bg-[#d92335] text-white py-3 rounded-lg font-semibold cursor-pointer transform hover:scale-[1.02] transition-all shadow-lg"
